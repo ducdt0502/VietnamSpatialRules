fce1d55c26d1891fa33e9e06a4f649a861bd0cb6
ducdt0502 <59598179+ducdt0502@users.noreply.github.com>
2020-02-13 16:38:19 +0700

    Update VietnamSpatialRules.java

--- VietnamSpatialRules.java
+++ VietnamSpatialRules.java
@@ -5,15 +5,35 @@ import com.graphhopper.routing.profiles.RoadAccess;
 import com.graphhopper.routing.util.spatialrules.DefaultSpatialRule;
 import com.graphhopper.routing.util.spatialrules.TransportationMode;
 
+public class VietnamSpatialRule extends DefaultSpatialRule {
+
 public double getMaxSpeed(String highwayTag, double _default) {
     // As defined in: https://wiki.openstreetmap.org/wiki/OSM_tags_for_routing/Maxspeed#Motorcar
     switch (highwayTag) {
         case "motorway":
-        case "trunk":
-            return Integer.MAX_VALUE;
+            return 120;
         case "residential":
             return 100;
-        case "living_street":
-            return 4;
+        case "tertiary":
+            return ;
+         case"secondary":
+            return ;
         default:
             return super.getMaxSpeed(highwayTag, _default);
+    }
+}
+     @Override
+    public RoadAccess getAccess(String highwayTag, TransportationMode transportationMode, RoadAccess _default) {
+        if (transportationMode == TransportationMode.MOTOR_VEHICLE) {
+            if (highwayTag.equals("track"))
+                return RoadAccess.DESTINATION;
+        }
+
+        return super.getAccess(highwayTag, transportationMode, _default);
+    }
+
+    @Override
+    public String getId() {
+        return Country.DEU.toString();
+    }
+}
